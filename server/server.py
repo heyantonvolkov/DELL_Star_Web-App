ad776735ca52b3670fa5fe8296a6996d56a6cbda
Anton <Tonyfirehead@gmail.com>
2020-06-28 15:31:39 +0300

    wiip

--- server/server.py
+++ server/server.py
@@ -1,11 +1,13 @@
 from flask_api import FlaskAPI
+from flask import request
 
 app = FlaskAPI(__name__)
 
 
 @app.route("/sortBubble", methods=['POST'])
-def index(num):
-	return num
+def sortBubble():
+	print(request.data)
+	return request.data
 
 # @app.route("/sortElse", methods=['GET', 'POST'])
 # def index(num):
