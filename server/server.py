443a875249bd74d9fa6fba3531186eee98549f8b
Anton <Tonyfirehead@gmail.com>
2020-06-28 15:18:32 +0300

    change

--- server/server.py
+++ server/server.py
@@ -3,16 +3,13 @@ from flask_api import FlaskAPI
 app = FlaskAPI(__name__)
 
 
-def fib(n):
-    a = 0
-    b = 1
-    for __ in range(n):
-        a, b = b, a + b
-    return a
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/sortBubble", methods=['POST'])
 def index(num):
 	return num
+
+# @app.route("/sortElse", methods=['GET', 'POST'])
+# def index(num):
+# 	return num
 
 
 if __name__ == "__main__":
