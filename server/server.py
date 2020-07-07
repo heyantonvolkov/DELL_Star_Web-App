98ada7b7ad1e82aeda5182027f6128ae817deb00
Anton <Tonyfirehead@gmail.com>
2020-07-07 12:01:58 +0300

    print request data

--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/sortBubble", methods=['POST'])
 def sortBubble():
+    print('DATA', request.data)
     collection = request.data.get('array')
     startTime = time.time()
     collection = provideBubbleSort(collection)
